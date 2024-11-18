65f765276de028856a4d7c0b002c379b8dfbc805
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-18 12:28:45 -0600

    section seven - java code generator

--- section-seven/Car.java
+++ section-seven/Car.java
@@ -12,34 +12,35 @@ public class Car {
     }
 
     public String getMake() {
-        return make;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String getColor() {
-        return color;
+        return this.make;
     }
 
     public void setMake(String make) {
         this.make = make;
     }
 
+    public double getPrice() {
+        return this.price;
+    }
+
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public int getYear() {
+        return this.year;
     }
 
     public void setYear(int year) {
         this.year = year;
     }
 
+    public String getColor() {
+        return this.color;
+    }
+
     public void setColor(String color) {
         this.color = color;
     }
+
 }
